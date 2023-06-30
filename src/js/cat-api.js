ccf4fb24376d83ae5189b3d28edb2383ac92d911
MizakaMaks <mizerniy.maksim@ukr.net>
2023-06-30 19:08:09 +0200

    try

---
+++ src/js/cat-api.js
@@ -0,0 +1,30 @@
+export {fetchBreeds, fetchCatBreed}
+
+const API_KEY = 'live_Ko6wZTeYmlx0ffigih2h1Z9ovtTKpVD4jIxy8RCsg0DzCnOULymC5Yb71xvzx8Tr'
+const BASE_URL = 'https://api.thecatapi.com/v1/breeds'
+
+function fetchBreeds() {
+    return fetch(`${BASE_URL}/breeds`, {
+    headers: {
+        'x-api-key': API_KEY,
+    }
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error(response.status)
+        }
+        return response.json()
+    })
+}
+
+function fetchCatBreed(catId) {
+    return fetch(`${BASE_URL}/images/search?breed_ids=${catId}`, {
+    headers: {
+        'x-api-key': API_KEY,
+    }
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error(response.status)
+        }
+        return response.json()
+    })
+}
